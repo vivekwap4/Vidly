2c9f8651f7fd84fffab2cac70e0488c2cd8c5613
Vivek Mishra <vivek.wap4@gmail.com>
2020-06-04 12:31:09 -0500

    Basic custom route

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -14,7 +14,31 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Shrek!" };
 
-            return View(movie);
+            //return View(movie);
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
+        }
+
+        public ActionResult Edit(int id) {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy) {
+            if (!pageIndex.HasValue) {
+                pageIndex = 1;
+            }
+
+            if (string.IsNullOrWhiteSpace(sortBy)) {
+                sortBy = "name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month) {
+            return Content(year + "/" + month);
         }
     }
 }
